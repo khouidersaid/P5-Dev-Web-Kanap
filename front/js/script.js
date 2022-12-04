fd03134b07a93c9903a9f43fd563ff7aaa34e522
unknown <mohammedsaidkhouider@gmail.com>
2022-12-04 19:10:34 +0100

    ajout de tout les produits dans la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,23 @@
-let pageproducts = {
-    fetch('http://localhost:3000/api/products');
-}
-    
-
-    
-    
-
-
+fetch("http://localhost:3000/api/products")
+  .then((res) => {
+    if (res.ok) {
+        return res.json();
+    }
+  })
+  .then((datas) => {
+    console.log(datas)
+    let html = ""
+    datas.forEach(element => {
+        html += `<a href="./product.html?id=${element.id}">
+        <article>
+          <img src=${element.imageUrl} alt="Lorem ipsum dolor sit amet, Kanap name1">
+          <h3 class="productName">${element.name}</h3>
+          <p class="productDescription">${element.description}</p>
+        </article>
+      </a>`
+    });
+    const section = document.getElementById("items")
+    section.innerHTML=html
+  })
+  .catch((err) => {});
 
