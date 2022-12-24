77c6f4221f761795eefe04bd03e988202abb7af9
unknown <mohammedsaidkhouider@gmail.com>
2022-12-24 11:32:33 +0100

    affichage de produit dans la page produits avec les détails et le choix de couleur

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,17 @@
+// Faire une demande à l'adresse "http://localhost:3000/api/products" et obtenir une réponse
 fetch("http://localhost:3000/api/products")
   .then((res) => {
     if (res.ok) {
+      // Si la réponse est "ok", la convertir en JSON et la retourner
         return res.json();
     }
   })
   .then((datas) => {
+    console.log(datas)
     let html = ""
+    // Pour chaque élément dans les données JSON
     datas.forEach(element => {
+      // Générer une chaîne HTML pour l'élément actuel en utilisant les données de l'élément
         html += `<a href="./product.html?id=${element._id}">
         <article>
           <img src=${element.imageUrl} alt="${element.altTxt}">
@@ -15,10 +20,14 @@ fetch("http://localhost:3000/api/products")
         </article>
       </a>`
     });
+    // Récupérer l'élément HTML avec l'id "items"
     const section = document.getElementById("items")
     section.innerHTML=html
   })
   .catch((err) => {
     const section = document.getElementById("items")
+    //Afficher un message d'érreur 
     section.innerHTML = `Une erreur est survenue (${(err)})`
   });
+
+
